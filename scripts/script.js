7e0813d9b54d41b5901198bc0430a9ae9729e3f9
lilycm <lilychakma733@gmail.com>
2024-02-14 11:37:13 +0600

    complete javascript part in home section

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,22 @@
 
-function play(){
+// function play(){
     // step -1: hide the home screen and to hide the screen add the class "hidden" to the home section
 
-    const homeSection = document.getElementById('home');
+    // const homeSection = document.getElementById('home');
 
-    homeSection.classList.add('hidden');
+    // homeSection.classList.add('hidden');
     // console.log(homeSection.classList);
 
 
     //show the play ground
-    const playGroundSection = document.getElementById('play-ground');
-    playGroundSection.classList.remove('hidden');
+    // const playGroundSection = document.getElementById('play-ground');
+    // playGroundSection.classList.remove('hidden');
 
+// }
+
+
+function play(){
+    hideElementById('home');
+    showElementById('play-ground');
 }
+
